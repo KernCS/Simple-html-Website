ca8c832f3088f5305c50a4a3f3e97ffdbbd16395
Kern <kerncs04@gmail.com>
2020-06-26 22:36:30 +0200

    IP.js fix

--- Scripts/ip.js
+++ Scripts/ip.js
@@ -1,15 +1,3 @@
-function display_c(){
-    var refresh=1000; // Refresh rate in milli seconds
-    mytime=setTimeout('display_ct()',refresh)
-}
-function display_ct() {
-    var x = new Date()
-    var x1=x.toUTCString();// changing the display to UTC string
-    document.getElementById('ct').innerHTML = x1;
-    tt=display_c();
-}
-
-
 var i = 0;
 var speed = 50;
 function typeWriter() {
